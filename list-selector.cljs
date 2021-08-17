19412fa7ba764bcd0fa0f181bdec81b03fad8df4
Steffen Glückselig <gist@gungfu.de>
2021-08-17 17:00:20 +0200

    state schmozzle

--- list-selector.cljs
+++ list-selector.cljs
@@ -4,18 +4,26 @@
 
 
 
+;; TODO: to work as :key later on - assert that :display-name is unique for user-input
 (defonce state (r/atom (->> (iterate inc 1)
-                         (map (fn [id] {:display-name (str "item " id)}))
+                         (map (fn [id] {:display-name (str "item " id) :selected true}))
                          (take 5))))
 
+(println @state)
 
-(defn hello []
-  [:> Box {:flexDirection "column" :borderStyle "round" :width 20 :paddingLeft 1 :paddingRight 1}
-   (for [{name :display-name} @state]
-     [:> Box {:key name }                                   ;; :borderStyle "round"
-      [:> Text "[ ] " name]])])
+(defn list-entry [item-state]
+  (let [!selected (r/atom (:selected item-state))
+        ;;input (useInput (fn input-hook [input, key] (cond (.-return key) (swap! !selected not))))
+        ]
+    [:> Box
+     [:> Text "[" (if (@!selected) "x" " ") "] " (:display-name item-state)]]))
+
+(defn app []
+  [:> Box {:flexDirection "column" :borderStyle "round" :width 20 :paddingX 1}
+   (for [item-state @state]
+     ^{:key (:display-name item-state)} [list-entry item-state])])
 
 
 
-(render (r/as-element [hello]))
+(render (r/as-element [app]))
 
